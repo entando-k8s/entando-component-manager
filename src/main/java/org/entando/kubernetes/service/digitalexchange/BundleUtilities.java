15d488f16b3c519ea173b35c793cd9c4d2c0d15c
Kerruba <luca.cherubin@gmail.com>
2020-03-09 15:43:54 +0100

    Makes the naming clearer

--- src/main/java/org/entando/kubernetes/service/digitalexchange/BundleUtilities.java
+++ src/main/java/org/entando/kubernetes/service/digitalexchange/BundleUtilities.java
@@ -5,20 +5,21 @@ import org.entando.kubernetes.model.debundle.EntandoDeBundle;
 
 public class BundleUtilities {
 
-    public static String getBundleVersionOrFail(EntandoDeBundle bundle, String version) {
+    public static String getBundleVersionOrFail(EntandoDeBundle bundle, String versionReference) {
        
-        String versionToFind = version;
-        if ( !isFormattedAsVersion(versionToFind)) {
-            versionToFind = (String) bundle.getSpec().getDetails().getDistTags().get(version);
+        String version = versionReference;
+        if ( !hasVersionFormat(versionReference)) {
+            version = (String) bundle.getSpec().getDetails().getDistTags().get(versionReference);
         }
-        if (Strings.isNullOrEmpty(versionToFind)) {
-            throw new RuntimeException("Invalid version '" + version + "' for bundle '" + bundle.getSpec().getDetails().getName() + "'");
+        if (Strings.isNullOrEmpty(version)) {
+            throw new RuntimeException("Invalid version '" + versionReference + "' for bundle '" + bundle.getSpec().getDetails().getName() + "'");
         }
-        return versionToFind;
+        return version;
         
     }
 
-    private static boolean isFormattedAsVersion(String versionToFind) {
+    private static boolean hasVersionFormat(String versionToFind) {
+        // Check if the provided string has version format ##.##.##
         return versionToFind.matches("\\d+\\.\\d+") || versionToFind.matches("\\d+\\.\\d+\\.\\d+");
     }
 }
