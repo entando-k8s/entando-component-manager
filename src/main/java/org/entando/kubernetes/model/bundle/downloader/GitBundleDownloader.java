4e76b45fcb805409bc4c80d4200e0d2f7d0bbedb
Kerruba <luca.cherubin@gmail.com>
2020-04-06 12:18:05 +0200

    Uses command line implementation of a git clone shallow not yet available on JGit

--- src/main/java/org/entando/kubernetes/model/bundle/downloader/GitBundleDownloader.java
+++ src/main/java/org/entando/kubernetes/model/bundle/downloader/GitBundleDownloader.java
@@ -2,8 +2,9 @@ package org.entando.kubernetes.model.bundle.downloader;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.entando.kubernetes.model.debundle.EntandoDeBundleTag;
@@ -13,16 +14,43 @@ public class GitBundleDownloader extends BundleDownloader{
     @Override
     protected Path saveBundleStrategy(EntandoDeBundleTag tag, Path targetPath) {
         try {
-            Git.cloneRepository()
-                    .setURI(tag.getTarball())
-                    .setBranch(tag.getVersion())
-                    .setCloneAllBranches(false)
-                    .setBranchesToClone(Collections.singletonList("refs/tags/" + tag.getVersion()))
-                    .setDirectory(targetPath.toFile())
-                    .call();
+            cloneUsingCliImplementation(tag, targetPath);
+
+//            cloneUsingJGitImplementation(tag, targetPath);
             return targetPath;
-        } catch (GitAPIException e) {
+        } catch (IOException | InterruptedException e) {
             throw new BundleDownloaderException("An error occurred while cloning git repo", e);
         }
+    }
+
+    private void cloneUsingCliImplementation(EntandoDeBundleTag tag, Path targetPath)
+            throws IOException, InterruptedException {
+        List<String> commands = new ArrayList<>();
+        String gitCommand = String.format( "git clone --branch %s --depth 1 %s %s",
+                tag.getVersion(),
+                tag.getTarball(),
+                targetPath.toAbsolutePath());
+        commands.add("/bin/sh");
+        commands.add("-c");
+        commands.add(gitCommand);
+
+        ProcessBuilder pb = new ProcessBuilder(commands);
+        pb.redirectErrorStream(true);
+        Process process = pb.start();
+
+        //EDIT:
+        // get Exit Status
+        if ( process.waitFor() != 0)
+            throw new BundleDownloaderException("An error occurred while shallow cloning the git repo");
+    }
+
+    private void cloneUsingJGitImplementation(EntandoDeBundleTag tag, Path targetPath) throws GitAPIException {
+        Git.cloneRepository()
+                .setURI(tag.getTarball())
+                .setBranch(tag.getVersion())
+                .setCloneAllBranches(false)
+                .setBranchesToClone(Collections.singletonList("refs/tags/" + tag.getVersion()))
+                .setDirectory(targetPath.toFile())
+                .call();
     }
 }
