f740fd878f66a68d0bc61f9b5ed28bbbe75db21b
Kerruba <luca.cherubin@gmail.com>
2020-06-23 18:14:37 +0200

    Adds more config to fix core

--- src/main/java/org/entando/kubernetes/config/CorsAccessControlConfiguration.java
+++ src/main/java/org/entando/kubernetes/config/CorsAccessControlConfiguration.java
@@ -3,7 +3,9 @@ package org.entando.kubernetes.config;
 import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,23 +15,30 @@ import java.util.List;
 
 @EnableWebMvc
 @Configuration
-@ConfigurationProperties(prefix = "cors.access.control")
+@ConfigurationProperties(prefix = "cors.access.control.allow")
 @Setter
 public class CorsAccessControlConfiguration implements WebMvcConfigurer {
 
-    private List<String> origin = Arrays.asList("*");
+    private List<String> origins = Arrays.asList("*");
     private List<String> headers = Arrays.asList("Content-Type", "Authorization");
     private List<String> methods = Arrays.asList("GET", "POST", "DELETE", "OPTIONS", "PATCH");
-    long maxAge = 3600;
+    private boolean credentials = true;
+
+    @Value("${cors.access.control.maxAge:3600}")
+    private long maxAge;
+
+    @Value("${cors.enabled:false}")
+    private boolean isCorsEnabled;
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins(origin.toArray(new String[0]))
-                .allowedHeaders(headers.toArray(new String[0]))
-                .allowedMethods(methods.toArray(new String[0]))
-                .maxAge(maxAge)
-                .allowCredentials(true);
-
+        if (isCorsEnabled) {
+            registry.addMapping("/**")
+                    .allowedOrigins(origins.toArray(new String[0]))
+                    .allowedHeaders(headers.toArray(new String[0]))
+                    .allowedMethods(methods.toArray(new String[0]))
+                    .maxAge(maxAge)
+                    .allowCredentials(credentials);
+        }
     }
 }
