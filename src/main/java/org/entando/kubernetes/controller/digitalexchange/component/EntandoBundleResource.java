7b583cdf2b4d81cfe194e8c44fa8199d87d95b24
firegloves <lucorset@gmail.com>
2020-12-16 15:37:29 +0100

    ENG-430 deleted erroneous tags on rest controllers that were generating ghost items in swagger ui

--- src/main/java/org/entando/kubernetes/controller/digitalexchange/component/EntandoBundleResource.java
+++ src/main/java/org/entando/kubernetes/controller/digitalexchange/component/EntandoBundleResource.java
@@ -14,10 +14,8 @@
 
 package org.entando.kubernetes.controller.digitalexchange.component;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import java.util.List;
 import org.entando.kubernetes.model.bundle.EntandoBundle;
 import org.entando.kubernetes.model.entandocore.EntandoCoreComponentUsage;
@@ -30,7 +28,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@OpenAPIDefinition(tags = {@Tag(name = "digital-exchange"), @Tag(name = "components")})
 @RequestMapping(value = "/components")
 public interface EntandoBundleResource {
 
