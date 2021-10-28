42e140d7a5b2d0bec903554cea7ab44589d45b8c
firegloves <lucorset@gmail.com>
2021-10-28 10:24:19 +0200

    ENG-2756 Lob postgres bug fix

--- src/main/java/org/entando/kubernetes/repository/InstalledEntandoBundleRepository.java
+++ src/main/java/org/entando/kubernetes/repository/InstalledEntandoBundleRepository.java
@@ -15,24 +15,20 @@ public interface InstalledEntandoBundleRepository extends JpaRepository<EntandoB
     @Transactional
     void deleteById(String id);
 
+    @Transactional
     List<EntandoBundleEntity> findAllByRepoUrlIn(List<String> repoUrls);
 
     default List<EntandoBundleEntity> findAllByRepoUrlInWithURLs(List<URL> repoUrls) {
-
-        System.out.println("####### CONVERTING LIST URL TYPE TO STRING ######");
-
         final List<String> repoUrlStrings = repoUrls.stream()
                 .map(URL::toString)
                 .collect(Collectors.toList());
         return findAllByRepoUrlIn(repoUrlStrings);
     }
 
+    @Transactional
     Optional<EntandoBundleEntity> findFirstByRepoUrl(String repoUrl);
 
     default Optional<EntandoBundleEntity> findFirstByRepoUrlWithUrl(URL repoUrl) {
-
-        System.out.println("####### CONVERTING URL TYPE TO STRING ######");
-
         return findFirstByRepoUrl(repoUrl.toString());
     }
 }
