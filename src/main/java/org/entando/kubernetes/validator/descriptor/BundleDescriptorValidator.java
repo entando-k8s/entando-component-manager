65da10aa3d51b3b9f07cfea140d9069f1570fddc
antromeo <45900043+antromeo@users.noreply.github.com>
2023-05-30 14:27:53 +0200

    ENG-4925: adds support to version v6 for bundle descriptor

--- src/main/java/org/entando/kubernetes/validator/descriptor/BundleDescriptorValidator.java
+++ src/main/java/org/entando/kubernetes/validator/descriptor/BundleDescriptorValidator.java
@@ -6,6 +6,8 @@ import java.util.Map;
 import java.util.function.Function;
 import javax.annotation.PostConstruct;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.KeyValue;
+import org.apache.commons.collections4.keyvalue.DefaultKeyValue;
 import org.entando.kubernetes.model.bundle.descriptor.BundleDescriptor;
 import org.entando.kubernetes.model.bundle.descriptor.DescriptorVersion;
 import org.springframework.stereotype.Component;
@@ -22,15 +24,18 @@ public class BundleDescriptorValidator extends BaseDescriptorValidator<BundleDes
     public void setupValidatorConfiguration() {
         setupValidatorConfigurationDescriptorV1();
         setupValidatorConfigurationDescriptorV5();
+        setupValidatorConfigurationDescriptorV6();
     }
 
     private void setupValidatorConfigurationDescriptorV1() {
-        Map<String, Function<BundleDescriptor, Object>> objectsThatMustNOTBeNull = new LinkedHashMap<>();
-        objectsThatMustNOTBeNull.put("components", BundleDescriptor::getComponents);
-        objectsThatMustNOTBeNull.put("code", BundleDescriptor::getCode);
+        var objectsThatMustNOTBeNull = createListFromPairs(
+                new DefaultKeyValue<>("components", BundleDescriptor::getComponents),
+                new DefaultKeyValue<>("code", BundleDescriptor::getCode)
+        );
 
-        Map<String, Function<BundleDescriptor, Object>> objectsThatMustBeNull = new LinkedHashMap<>();
-        objectsThatMustBeNull.put("name", BundleDescriptor::getName);
+        var objectsThatMustBeNull = createListFromPairs(
+                new DefaultKeyValue<>("name", BundleDescriptor::getName)
+        );
 
         addValidationConfigMap(DescriptorVersion.V1,
                 List.of(super::validateDescriptorFormatOrThrow),
@@ -39,16 +44,46 @@ public class BundleDescriptorValidator extends BaseDescriptorValidator<BundleDes
     }
 
     private void setupValidatorConfigurationDescriptorV5() {
-        Map<String, Function<BundleDescriptor, Object>> objectsThatMustNOTBeNull = new LinkedHashMap<>();
-        objectsThatMustNOTBeNull.put("components", BundleDescriptor::getComponents);
-        objectsThatMustNOTBeNull.put("name", BundleDescriptor::getName);
+        var objectsThatMustNOTBeNull = createListFromPairs(
+                new DefaultKeyValue<>("components", BundleDescriptor::getComponents),
+                new DefaultKeyValue<>("name", BundleDescriptor::getName)
+        );
 
-        Map<String, Function<BundleDescriptor, Object>> objectsThatMustBeNull = new LinkedHashMap<>();
-        objectsThatMustBeNull.put("code", BundleDescriptor::getCode);
+        var objectsThatMustBeNull = createListFromPairs(
+                new DefaultKeyValue<>("code", BundleDescriptor::getCode)
+        );
 
         addValidationConfigMap(DescriptorVersion.V5,
                 List.of(super::validateDescriptorFormatOrThrow),
                 objectsThatMustNOTBeNull,
                 objectsThatMustBeNull);
     }
+
+    private void setupValidatorConfigurationDescriptorV6() {
+        var fieldsThatMustNOTBeNull = createListFromPairs(
+                new DefaultKeyValue<>("components", BundleDescriptor::getComponents),
+                new DefaultKeyValue<>("name", BundleDescriptor::getName)
+        );
+
+        var fieldsThatMustBeNull = createListFromPairs(
+                new DefaultKeyValue<>("code", BundleDescriptor::getCode)
+        );
+
+        addValidationConfigMap(DescriptorVersion.V6,
+                List.of(super::validateDescriptorFormatOrThrow),
+                fieldsThatMustNOTBeNull,
+                fieldsThatMustBeNull);
+    }
+
+
+    private Map<String, Function<BundleDescriptor, Object>> createListFromPairs(KeyValue<String, Function<BundleDescriptor, Object>>... pairs) {
+        Map<String, Function<BundleDescriptor, Object>> list = new LinkedHashMap<>();
+        for (KeyValue<String, Function<BundleDescriptor, Object>> pair : pairs) {
+            String key = pair.getKey();
+            Function<BundleDescriptor, Object> getter = pair.getValue();
+            list.put(key, getter);
+        }
+        return list;
+    }
+
 }
