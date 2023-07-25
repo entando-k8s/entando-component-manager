588496d3a9d84f47bec720cb3f9fae3bf2b9151d
antromeo <45900043+antromeo@users.noreply.github.com>
2023-07-25 09:45:55 +0200

    ENG-5063: adds log message

--- src/main/java/org/entando/kubernetes/config/tenant/TenantConfig.java
+++ src/main/java/org/entando/kubernetes/config/tenant/TenantConfig.java
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.List;
 import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
 import org.entando.kubernetes.exception.EntandoComponentManagerException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -16,6 +17,7 @@ import org.springframework.context.annotation.Profile;
 @Configuration
 @Profile("!test")
 @Setter
+@Slf4j
 public class TenantConfig {
 
     public String tenantConfigs;
@@ -37,6 +39,7 @@ public class TenantConfig {
         if (tenantConfigs != null) {
             try {
                 tenantConfigList = objectMapper.readValue(tenantConfigs, new TypeReference<List<TenantConfigDTO>>() {});
+                log.info("Tenant configurations have been parsed successfully");
             } catch (final IOException e) {
                 throw new EntandoComponentManagerException(e);
             }
