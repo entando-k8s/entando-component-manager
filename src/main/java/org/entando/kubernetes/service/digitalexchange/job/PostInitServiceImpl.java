55f4bb1c26b8fdf3ed0f3ff644c61e98f3e68783
Riccardo Fadda <r.fadda@entando.com>
2023-07-25 18:47:26 +0200

    IT-411 updated multi-arch docker image for App Builder menu

--- src/main/java/org/entando/kubernetes/service/digitalexchange/job/PostInitServiceImpl.java
+++ src/main/java/org/entando/kubernetes/service/digitalexchange/job/PostInitServiceImpl.java
@@ -75,8 +75,8 @@ public class PostInitServiceImpl implements PostInitService, InitializingBean {
         List<PostInitItem> items = new ArrayList<>();
         items.add(PostInitItem.builder()
                 .name("entando-epc-bootstrap")
-                .url("docker://registry.hub.docker.com/entando/entando-bootstrap-bundle")
-                .version("1.0.1")
+                .url("docker://registry.hub.docker.com/riccardofadda/entando-bootstrap-bundle")
+                .version("1.1.0-IT-422-PR-11")
                 .action(ACTION_INSTALL_OR_UPDATE)
                 .priority(1)
                 .build());
