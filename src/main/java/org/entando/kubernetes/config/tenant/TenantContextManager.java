26a74e3505b6dab57a278e20558740b0d7e08d08
Alessio Pintus <alessio.pintus@gmail.com>
2023-08-01 16:55:22 +0200

    ENG-5057 Code improvements

--- src/main/java/org/entando/kubernetes/config/tenant/TenantContextManager.java
+++ src/main/java/org/entando/kubernetes/config/tenant/TenantContextManager.java
@@ -1,15 +1,16 @@
 package org.entando.kubernetes.config.tenant;
 
 import java.util.Objects;
+import lombok.experimental.UtilityClass;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
+@UtilityClass
 public class TenantContextManager {
-
     public static final String KEY_TENANT_CODE = "TENANT_CODE";
 
     public static void setTenantCode(String tenantCode) {
-        Objects.requireNonNull(RequestContextHolder.getRequestAttributes())
+        Objects.requireNonNull(RequestContextHolder.currentRequestAttributes())
                 .setAttribute(KEY_TENANT_CODE, tenantCode, RequestAttributes.SCOPE_REQUEST);
     }
 
