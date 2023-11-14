5e6388e8120dd7e41453c8446a875ad067444e94
Matteo E. Minnai <m.minnai@entando.com>
2023-11-14 18:55:22 +0100

    ENG-5083: fix unused imports

--- src/main/java/org/entando/kubernetes/config/tenant/TenantDataSourceConfiguration.java
+++ src/main/java/org/entando/kubernetes/config/tenant/TenantDataSourceConfiguration.java
@@ -18,7 +18,6 @@ import org.springframework.boot.jdbc.DatabaseDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.ResourceLoader;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 
